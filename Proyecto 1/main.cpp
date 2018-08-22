325401d38ac8c673c806eb4c27d8192a5baf9166
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:13:15 -0500

    cambios

--- Proyecto 1/main.cpp
+++ Proyecto 1/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello Rolinson :V xD" << endl;
+    int a;
+    int b;
+    int z;
+    cin >> a;
+    cin >> b;
+    z = a + b;
+    cout << "La suma de " << a << " y " << b << " es igual a " << z << endl;
     return 0;
 }
